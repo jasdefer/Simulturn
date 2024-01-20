a334e6425473595a344d86a88aa0ce7a2bb6aeeb
Justus Bonz <justus.bonz@uni-hamburg.de>
2024-01-20 22:41:13 +0100

    Start adding more validation helper tests

--- Simulturn/Test/SimulturnDomain.UnitTests/Logic/ValidationHelperTests.cs
+++ Simulturn/Test/SimulturnDomain.UnitTests/Logic/ValidationHelperTests.cs
@@ -1,5 +1,7 @@
-﻿using SimulturnDomain.Helper;
+﻿using SimulturnDomain.DataStructures;
+using SimulturnDomain.Helper;
 using SimulturnDomain.Logic;
+using SimulturnDomain.Model;
 using SimulturnDomain.ValueTypes;
 
 namespace SimulturnDomain.UnitTests.Logic;
@@ -29,5 +31,31 @@ public class ValidationHelperTests
         };
         var totalRequiredSpace = ValidationHelper.GetRequiredSpace(requiredSpace, armyMap.ToHexMap(), trainingMap.ToHexMap());
         totalRequiredSpace.Should().Be(12);
+    }
+
+    [Fact]
+    public void Test3()
+    {
+        var coordinates = new Coordinates(1, 2);
+        var order = new Structure();
+        var turnMap = new Dictionary<ushort, IDictionary<Coordinates, Structure>>();
+        var count = ValidationHelper.GetConstructionCount(3, coordinates, order,  turnMap.ToTurnHexMap());
+        count.Should().Be(0);
+    }
+
+    [Fact]
+    public void Test4()
+    {
+        var coordinates = new Coordinates(1, 1);
+        var order = new Structure(3, 7);
+        var turnMap = new Dictionary<ushort, IDictionary<Coordinates, Structure>>()
+        {
+            {0, new Dictionary<Coordinates, Structure>(){ { coordinates, new Structure(2) } } },
+            {2, new Dictionary<Coordinates, Structure>(){ { coordinates, new Structure(3) } } },
+            {3, new Dictionary<Coordinates, Structure>(){ { coordinates, new Structure(4) } } },
+            {4, new Dictionary<Coordinates, Structure>(){ { coordinates, new Structure(5) }, { new Coordinates(1, 2), new Structure(6) } } },
+        };
+        var count = ValidationHelper.GetConstructionCount(3, coordinates, order, turnMap.ToTurnHexMap());
+        count.Should().Be(19);
     }
 }
