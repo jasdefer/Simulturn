4d9608ae0a84db38d5eb4d4345b0b22d9dc709e4
Justus Bonz <justus.bonz@uni-hamburg.de>
2024-01-24 21:49:47 +0100

    Prepare AI

---
+++ Simulturn/Source/SimulturnDomain/Ai/IAi.cs
@@ -0,0 +1,15 @@
+﻿using SimulturnDomain.DataStructures;
+using SimulturnDomain.Model;
+using SimulturnDomain.Settings;
+using SimulturnDomain.ValueTypes;
+
+namespace SimulturnDomain.Ai;
+public interface IAi
+{
+    Order GetOrder(ushort turn,
+                   PlayerState playerState,
+                   GameSettings gameSettings,
+                   TurnMap<HexMap<PlayerMap<Fight>>> fightTurnPlayerMap,
+                   TurnMap<HexMap<PlayerMap<Army>>> armyTurnHexPlayerMap,
+                   TurnMap<HexMap<PlayerMap<Structure>>> structureTurnHexPlayerMap);
+}
